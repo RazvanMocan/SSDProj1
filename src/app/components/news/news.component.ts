b9b931765f03d3728705f64724beb96f99547233
Razvan Mocan <razvanmocan15@gmail.com>
2018-12-02 12:58:26 +0200

    Integrate newest widgests with the backend.

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-heroes:string[]=["Object1","Object2","Object3"];
-rating=5;
-  constructor() {
+heroes: string[];
+ids: string[] = ['idnews1', 'idnews2', 'idnews3'];
+rating = 5;
+
+  constructor(private  httpClient: HttpClient) {
 
   }
 
   ngOnInit() {
-
+    this.httpClient.get('http://localhost:7070/api/newest').subscribe(
+      (value: any[]) => {
+        console.log(this.heroes);
+        console.log(value);
+        this.heroes = value;
+        console.log(this.heroes === value);
+      }
+    );
   }
 
 }
