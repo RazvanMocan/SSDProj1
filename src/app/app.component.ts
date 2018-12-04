f1bb32fa6d3a569175f03e5e9baa0caf9204f33d
Andrei <andrei_bulzan2005@yahoo.coom>
2018-12-04 17:48:40 +0200

    ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 })
 export class AppComponent {
   title = 'SSDProj';
-  tiles= [
+  isAdmin = 1;
+  tiles = [
     {text: 'One', cols: 3, rows: 3, color: 'lightblue'},
     {text: 'Two', cols: 3, rows: 3, color: 'lightgreen'},
     {text: 'Three', cols: 3, rows: 3, color: 'lightpink'},
@@ -23,7 +24,29 @@ export class AppComponent {
     {text: 'Three', cols: 3, rows: 3, color: 'lightpink'},
     {text: 'Four', cols: 3, rows: 3, color: '#DDBDF1'},
   ];
-  rating=5;
+  tilesUsers = [
+    {text: 'User1', cols: 3, rows: 3, color: 'lightblue'},
+    {text: 'User2', cols: 3, rows: 3, color: 'lightgreen'},
+    {text: 'User3', cols: 3, rows: 3, color: 'lightpink'},
+    {text: 'User4', cols: 3, rows: 3, color: 'lightblue'},
+    {text: 'User5', cols: 3, rows: 3, color: 'lightgreen'},
+    {text: 'User6', cols: 3, rows: 3, color: 'lightpink'},
+    {text: 'User7', cols: 3, rows: 3, color: 'lightblue'},
+    {text: 'User8', cols: 3, rows: 3, color: 'lightgreen'},
+    {text: 'User9', cols: 3, rows: 3, color: 'lightpink'}
+  ];
+  rating = 5;
   public myVar: string;
-
+getStyle()
+{
+  if (this.isAdmin==0)
+    return -5;
+  return 5;
+}
+  getStyle2()
+  {
+    if (this.isAdmin!=0)
+      return -5;
+    return 5;
+  }
 }
